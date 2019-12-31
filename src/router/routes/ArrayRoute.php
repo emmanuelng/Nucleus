b7ec9ab0820ec84bc2c746fd6b2d34b0f2d09a0e
Emmanuel Ng <emmanuel_ng@outlook.com>
2019-12-30 22:13:34 -0500

    Adds the NEON notation

--- src/router/routes/ArrayRoute.php
+++ src/router/routes/ArrayRoute.php
@@ -10,39 +10,10 @@ use Nucleus\Router\Response;
 use Nucleus\Router\Route;
 
 /**
- * This class loads a route from a JSON file.
- *
- * - **method**: The request method.
- * - **url**: The URL
- * - **parameters**: The parameters schema
- * - **request**: The request schema
- * - **response**: The response schema
- * - **callback**: The callback. A callback must be a valid callable which
- *   takes a `Request` and a `Response` as parameters.
- *   Example:
- *   ```
- *   function myCallback(Request $res, Response $res): void {...}
- *   ```
- *
- * **Example file**: myRoute.json
- * ```
- * {
- *      'method': 'GET',
- *      'url': '/users',
- *      'parameters': {
- *          'user-id': {'type': 'string', 'default': ''}
- *      },
- *      'response': {
- *          'users': {
- *              'type': {'username': {'type': 'string'}},
- *              'isList': true
- *          }
- *      },
- *      'callback': 'User::get'
- * }
- * ```
+ * Concrete class of the Route interface allowing to define routes using
+ * arrays.
  */
-class RouteFile implements Route
+class ArrayRoute implements Route
 {
     /**
      * The request method.
@@ -89,50 +60,50 @@ class RouteFile implements Route
     /**
      * Initializes the route file.
      *
-     * @param string $filePath The path
+     * @param string $route The route array.
      */
-    public function __construct(string $filePath)
+    public function __construct(array $route)
     {
-        // Read file
-        $routeArr = json_decode(file_get_contents($filePath), true);
-        if ($routeArr === null) {
-            throw new InvalidRouteException('Invalid file content.');
-        }
-
         // Set method
-        $this->method = $routeArr['method'] ?? '';
+        $this->method = $route['method'] ?? '';
         if (empty($this->method) || !is_string($this->method)) {
-            throw new InvalidRouteException('Invalid method.');
+            $msg = 'Invalid or missing method.';
+            throw new InvalidRouteException($msg);
         }
 
         // Set URL
-        $this->url = $routeArr['url'] ?? '';
+        $this->url = $route['url'] ?? '';
         if (empty($this->url) || !is_string($this->url)) {
-            throw new InvalidRouteException('Invalid URL.');
+            $msg = 'Invalid or missing URL.';
+            throw new InvalidRouteException($msg);
         }
 
         // Set parameter's schema
-        $this->parameters = $routeArr['parameters'] ?? [];
+        $this->parameters = $route['parameters'] ?? [];
         if (!is_array($this->parameters)) {
-            throw new InvalidRouteException('Invalid parameter schema.');
+            $msg = 'Invalid or missing parameter schema.';
+            throw new InvalidRouteException($msg);
         }
 
         // Set request schema
-        $this->requestBody = $routeArr['request'] ?? [];
+        $this->requestBody = $route['request'] ?? [];
         if (!is_array($this->requestBody)) {
-            throw new InvalidRouteException('Invalid request schema.');
+            $msg = 'Invalid or missing request schema.';
+            throw new InvalidRouteException($msg);
         }
 
         // Set response schema
-        $this->responseBody = $routeArr['response'] ?? [];
+        $this->responseBody = $route['response'] ?? [];
         if (!is_array($this->responseBody)) {
-            throw new InvalidRouteException('Invalid response schema.');
+            $msg = 'Invalid or missing response schema.';
+            throw new InvalidRouteException($msg);
         }
 
         // Set callback
-        $this->callback = $routeArr['callback'] ?? '';
+        $this->callback = $route['callback'] ?? '';
         if (!is_callable($this->callback)) {
-            throw new InvalidRouteException('Invalid callback.');
+            $msg = 'Invalid or missing callback.';
+            throw new InvalidRouteException($msg);
         }
     }
 
